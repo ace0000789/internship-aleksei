f5d1e66724df5494b367df54143acc5e94b9d9eb
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2024-01-11 18:42:16 +0100

    Update init-submenu.js

--- source/js/init-submenu.js
+++ source/js/init-submenu.js
@@ -2,6 +2,10 @@ export function initSubMenu() {
   const subButtons = document.querySelectorAll('[data-validate="header-subbutton"]');
   const subMenus = document.querySelectorAll('[data-validate="header-submenu"]');
 
+  if (!subButtons || !subMenus) {
+    return;
+  }
+
   subButtons.forEach((subButton, index) => {
     const subMenu = subMenus[index]; // Получаем соответствующий subMenu по индексу
 
