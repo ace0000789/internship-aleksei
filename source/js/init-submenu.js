2d9c2d9d48a351c0009356c1ff2d882198641d81
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2024-01-11 15:09:54 +0100

    add js for sub menu

--- source/js/init-submenu.js
+++ source/js/init-submenu.js
@@ -3,16 +3,16 @@ export function initSubMenu() {
   const subButton = document.querySelector('[data-validate="header-subbutton"]');
   const subMenu = document.querySelector('[data-validate="header-submenu"]');
 
-  subButton.addEventListener('click', () => {
-    subMenu.classList.add('header__submenu-is-open');
-  });
 
-  document.addEventListener('click', (event) => {
-    if (!subButton.contains(event.target) && !subMenu.contains(event.target)) {
+  subButton.addEventListener('click', subMenuToggle);
+
+  function subMenuToggle() {
+    if (subMenu.classList.contains('header__submenu-is-close')) {
+      subMenu.classList.remove('header__submenu-is-close');
+      subMenu.classList.add('header__submenu-is-open');
+    } else {
+      subMenu.classList.add('header__submenu-is-close');
       subMenu.classList.remove('header__submenu-is-open');
     }
-  });
-
-
-  
+  }
 }
