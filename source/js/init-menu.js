4c8c099b86acbc610f7a95d691acbde3fb4e38da
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2024-01-11 12:32:48 +0100

    header button open / close work

--- source/js/init-menu.js
+++ source/js/init-menu.js
@@ -1,10 +1,9 @@
 export function initMenu() {
   const header = document.querySelector('[data-validate="header"]');
   const headerButton = document.querySelector('[data-validate="header-button"]');
-  const wrapper = document.querySelector('[data-validate="wrapper"]');
   const html = document.querySelector('[data-validate="html"]');
 
-  if (!header || !headerButton || !wrapper) {
+  if (!header || !headerButton) {
     return;
   }
 
@@ -14,12 +13,10 @@ export function initMenu() {
     if (header.classList.contains('is-closed')) {
       header.classList.remove('is-closed');
       header.classList.add('is-opened');
-      wrapper.classList.add('is-bg');
       html.classList.add('scroll-lock');
     } else {
       header.classList.add('is-closed');
       header.classList.remove('is-opened');
-      wrapper.classList.remove('is-bg');
       html.classList.remove('scroll-lock');
     }
   }
