5b2de02038c85542a8ebae0565b53667e0037e9d
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2024-01-24 13:12:14 +0100

    start swiper programs

--- source/js/main.js
+++ source/js/main.js
@@ -31,3 +31,38 @@ window.addEventListener('DOMContentLoaded', () => {
     form.init();
   });
 });
+/*
+//.forEach() // метод перебора массива
+
+const fruits = ['banana', 'apple', 'lemon', 'orange'];
+fruits.forEach((value) => {
+  console.log(value);
+});
+
+//.map() // Метод преобразования массива в другой (callback)
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const squares = nums.map(function (z) {
+  return z * z
+})
+console.log(squares)
+
+
+const titles = ['Die hard', 'Terminator']; // find - метод поиска в массиве
+const favoriteFilmTitle = titles.find((title) => title.includes('hard'));
+console.log(favoriteFilmTitle); // 'Die hard'
+
+const numbers = [11, 12, 13, 15, 100]; // метод every проверяет если все элементы соответствуеют условию вернет true
+const isEveryNumberOverTen = numbers.every((value) => {
+  return value > 10;
+});
+console.log(isEveryNumberOverTen);
+
+
+const array1 = [1, 2, 3, 4]; // reduce -- перебирает массив (cумма)
+const initialValue = 0;
+const sumWithInitial = array1.reduce(
+(accumulato, currentValue) => accumulato + currentValue, initialValue
+);
+console.log(sumWithInitial);
+*/
