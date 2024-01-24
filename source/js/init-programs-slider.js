2e4e29c39aa777dd55c7262d56534de83a7a48bd
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2024-01-24 15:20:15 +0100

    start arrows

--- source/js/init-programs-slider.js
+++ source/js/init-programs-slider.js
@@ -1,18 +1,24 @@
 import Swiper from 'swiper';
-import {Navigation} from 'swiper/modules';
+import {Navigation, Scrollbar} from 'swiper/modules';
 
 function initProgramsSlider() {
   const sliderMySlider = new Swiper('[data-validate="swiper-programs"]', {
-    modules: [Navigation],
+    modules: [Navigation, Scrollbar],
 
     navigation: {
       nextEl: '[data-validate="swiper-programs-next"]',
       prevEl: '[data-validate="swiper-programs-prev"]',
     },
 
+    scrollbar: {
+      el: '[data-validate="swiper-programs-scrollbar"]',
+      hide: false,
+      dragSize: 392,
+    },
+
     grabCursor: true,
     autoHeight: true,
-    loop: true,
+    loop: false,
     breakpoints: {
       1440: {
         slidesPerView: 3,
