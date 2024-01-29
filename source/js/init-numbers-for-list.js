f885d0d0ed5f23ed5bd7868830555ed14a7ccab3
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2024-01-29 16:01:10 +0100

    js check exist

--- source/js/init-numbers-for-list.js
+++ source/js/init-numbers-for-list.js
@@ -1,11 +1,14 @@
 export function initNumbersForList() {
-
   const listItems = document.querySelectorAll('.grant__item');
+
+  if (!listItems.length) {
+    return;
+  }
+
   let number = 1;
   listItems.forEach((el)=> {
     const span = el.querySelector('.grant__number');
     span.innerText = number;
     number++;
   });
-
 }
