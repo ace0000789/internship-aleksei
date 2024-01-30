4e1f57240433cbd4b7cda4c5c1ba9fdda89ac6d0
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2024-01-30 16:23:59 +0100

    work 2 form

--- source/js/vendor/form-validate/callback.js
+++ source/js/vendor/form-validate/callback.js
@@ -1,31 +1,41 @@
-
 const baseSuccessCallback = (event) => {
   event.preventDefault();
-  // В данном колбеке бэкендер, либо разработчик при необходимости будет писать запрос на отправку формы на сервер и обрабатывать возможные ошибки или успешную отправку формы на сервер
+
   const url = 'https://echo.htmlacademy.ru/';
-  const formData = new FormData(document.querySelector('.modal__form-container form'));
 
-  fetch(url, {
-    method: 'POST',
-    body: formData,
-  }).then((response) => response.text()).then((data) => {
-    document.body.innerHTML = `<pre>${data}</pre>`;
-  }).catch(() => {
-    document.body.innerHTML = 'Ошибка: не удалось отправить форму на сервер';
-  });
+  if (event.target.dataset.form === 'modal') {
+    const formData = new FormData(event.target);
 
+    fetch(url, {
+      method: 'POST',
+      body: formData,
+    }).then((response) => response.text()).then((data) => {
+      document.body.innerHTML = `<pre>${data}</pre>`;
+    }).catch(() => {
+      document.body.innerHTML = 'Ошибка: не удалось отправить форму на сервер';
+    });
+  } else if (event.target.dataset.form === 'feedback') {
+    const formData2 = new FormData(event.target);
+
+    fetch(url, {
+      method: 'POST',
+      body: formData2,
+    }).then((response) => response.text()).then((data) => {
+      document.body.innerHTML = `<pre>${data}</pre>`;
+    }).catch(() => {
+      document.body.innerHTML = 'Ошибка: не удалось отправить форму на сервер';
+    });
+  }
 };
 
 const baseErrorCallback = (event) => {
   event.preventDefault();
-  // Данный коллбек используется при необходимости выполнить какое-либо действие помимо показа ошибок при попытке отправить неккорректные данные, он не связан с запросами на сервер
+
 };
 
 export const callbacks = {
   base: {
-    // Сбросс формы
     reset: true,
-    // Таймаут сброса формы
     resetTimeout: 500,
     successCallback: baseSuccessCallback,
     errorCallback: baseErrorCallback,
