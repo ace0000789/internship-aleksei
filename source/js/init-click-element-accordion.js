a90ddc7efa90806decf8bde25935603104a0a4a0
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2024-02-01 17:33:36 +0100

    resolve tabs click on tabs

--- source/js/init-click-element-accordion.js
+++ source/js/init-click-element-accordion.js
@@ -1,15 +1,14 @@
 const infoItem = document.querySelectorAll('.faq__item');
-const button = document.querySelector('faq__button');
 
 export function initClickElementAccordion() {
 
-  if (!infoItem || infoItem.length === 0 || button) {
+  if (!infoItem || infoItem.length === 0) {
     return;
   }
 
   infoItem.forEach((el) => {
     el.addEventListener('click', (e) => {
-      if (e.target.classList.contains(button)) {
+      if (e.target.classList.contains('faq__button')) {
         return;
       }
       const buttons = el.querySelectorAll('.faq__button');
