ed72d7ee9e6e0628b63329b18764ec191750eb71
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2024-02-05 13:12:22 +0100

    delete cmment

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,3 @@
-// https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-//import 'swiper/css';
-
 import { stickyHeader } from './stiky-header';
 import { initMenu } from './init-menu';
 import { initAccordions } from './init-accordion';
